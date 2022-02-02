99f57b25b71fff0883184ccc2cf9857d5a88cba6
Matt0105 <matteo0105@gmail.com>
2022-02-02 15:42:10 +0100

    db populated

--- index-php.php
+++ index-php.php
@@ -1,6 +1,6 @@
 <?php 
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,12 +17,14 @@
     <?php include_once __DIR__ . '/partials/header.php' ?>
 
     <!-- main -->
+    <?php include_once __DIR__ . '/server/db.php' ?>
     <main>
         <div class="card-container">
             <div class="card">
-                <img src="https://picsum.photos/200/300" alt="">
+                <img src="http://<?php echo $_SERVER['HTTP_HOST'] ?>/php-ajax-dischi/img/<?php echo $musicData[0]["img"] ?>" alt="">
                 <h2>titolone</h2>
-                <p>paragrafone</p>
+                <p class="author">paragrafone</p>
+                <p class="year">anno</p>
             </div>
           
             </div>
---
+++ server/db.php
@@ -0,0 +1,77 @@
+<?php 
+
+    $musicData = [
+        [
+            "title" => "new jersey",
+            "author" => "Bon Jovi",
+            "year" => "1988",
+            "genre" => "Rock",
+            "img" => "bon-jovi-new-jersey.jpeg"
+        ],
+        [
+            "title" => "live at wembley",
+            "author" => "Queen",
+            "year" => "1992",
+            "genre" => "Rock",
+            "img" => "queen.jpeg"
+        ],
+        [
+            "title" => "ten summoner's tales",
+            "author" => "Sting",
+            "year" => "1993",
+            "genre" => "Pop",
+            "img" => "sting.jpg"
+        ],
+        [
+            "title" => "steve gadd band",
+            "author" => "Steve Gadd Band",
+            "year" => "2018",
+            "genre" => "Jazz",
+            "img" => "steve.jpg"
+        ],
+        [
+            "title" => "brave new world",
+            "author" => "Iron Maiden",
+            "year" => "2000",
+            "genre" => "Metal",
+            "img" => "iron.jpeg"
+        ],
+        [
+            "title" => "one more car, one more raider",
+            "author" => "Eric Clapton",
+            "year" => "2002",
+            "genre" => "Rock",
+            "img" => "eric.jpg"
+        ],
+        [
+            "title" => "made in japan",
+            "author" => "Deep Purple",
+            "year" => "1972",
+            "genre" => "Rock",
+            "img" => "purple.jpeg"
+        ],
+        [
+            "title" => "and justice for all",
+            "author" => "Metallica",
+            "year" => "1988",
+            "genre" => "Metal",
+            "img" => "metallica.jpeg"
+        ],
+        [
+            "title" => "hard wired",
+            "author" => "Dave Weckl",
+            "year" => "1994",
+            "genre" => "Jazz",
+            "img" => "wired.jpg"
+        ],
+        [
+            "title" => "bad",
+            "author" => "Michael Jackson",
+            "year" => "1987",
+            "genre" => "Pop",
+            "img" => "bad.jpg"
+        ]
+        
+    ];
+
+?>
