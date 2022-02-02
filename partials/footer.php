d0be016d419b0ac7afffa6b80edb189c77eaf74b
Matt0105 <matteo0105@gmail.com>
2022-02-02 14:33:41 +0100

    scaffholding + header and footer

---
+++ partials/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer-container">
+        <span>Copyrights 2022</span>
+    </div>
+</footer>
---
+++ partials/header.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="header-container">
+        <img src="http://<?php echo $_SERVER['HTTP_HOST'] ?>/php-ajax-dischi/img/17-176153_download-logo-spotify-png.png" alt="">
+        
+    </div>
+</header>
